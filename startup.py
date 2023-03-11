bed431373d95781b95c9311229e4adb09f9a035a
rokujiin <112463409+rokujiin@users.noreply.github.com>
2023-03-11 01:56:37 -0800

    Update startup.py
    added classes to improve readability and efficiency

--- startup.py
+++ startup.py
@@ -1,52 +1,48 @@
 import os
 import time
-from datetime import datetime
 
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+class Startup:
 
+    def __init__(self):
+        self.name = "Rey"
+        self.start()
 
-
-
-print('Welcome back, Rey!')
-print('')
-print("Today's date is: ", dt_string)
-print('')
-start = input("Would you like to start your overlay, y or n? ")
-
-
-
-
-#whileuser input is not y or n, keep asking
-
-while start != "y"  and start != "n" and start != "Y" and start != "N":
-    start = input("Please enter y or n: ")
+    def start(self):
+        print("Welcome back, " + self.name + "!")
+        print("")
+        print("The time is: " + time.strftime("%H:%M:%S"))
+        print("")
+        self.overlay()
     
-if start == "n" or start == "N":
-    exit()
-    
-elif start == "y" or start == "Y":   
-    print("Overlay starting in: ")
         
-    countdown = 3
-    while countdown > 0:
-        print(countdown)
-        countdown = countdown - 1
-        time.sleep(1)
-                   
-        if countdown == 0:
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-            print(response)
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-            print(response)
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.2.3.lnk")
-            print(response)
-            response = os.startfile(r"H:\IDEs\Microsoft VS Code\bin\code.cmd")
-            print(response)
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HWiNFO64\HWiNFO64.lnk")
-            print(response)
-            response = os.startfile(r"C:\Users\rheyp\Desktop\Discord.lnk")
-            print(response)
-          
-            break
-            
+    def overlay(self):
+        start = input("Would you like to start your overlay, y or n? ")
+        while start != "y" and start != "n":
+            start = input("Would you like to start your overlay, y or n? ")
+        if start == "n":
+            exit()
+        elif start == "y":
+            print("Overlay starting in: ")
+            countdown = 3
+            while countdown > 0:
+                print(countdown)
+                countdown = countdown - 1
+                time.sleep(1)
+                if countdown == 0:
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.2.3.lnk")
+                    print(response)
+                    response = os.startfile(r"H:\IDEs\Microsoft VS Code\bin\code.cmd")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HWiNFO64\HWiNFO64.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\Users\rheyp\Desktop\Discord.lnk")
+                    print(response)
+                    break
+                
+if __name__ == "__main__":
+    Startup()
+    
