64bc060e0199d930f6f6326d2f33e6b9b5bf53e1
Rey Philip Louise Halili <112463409+rokujiin@users.noreply.github.com>
2023-03-17 04:47:42 -0700

    Update 2import os.py

--- 2import os.py
+++ 2import os.py
@@ -1,60 +1,52 @@
 import os
 import time
-from tracemalloc import start
-from urllib import response
-
-print("Welcome back Rey")
-print("")
+import subprocess
 
 
-start = input("Would you like to start your overlay, y or n? ")
+class Startup:
 
-#while user input is not equal to y or n, keep asking
-while start != "y" and start != "n":
-    start = input("Would you like to start your overlay, y or n? ")
+    def __init__(self):
+        self.ampm = time.strftime("%p")
+        self.name = "Rey"
+        self.start()
+
+    def start(self):
+        print("Welcome back, " + self.name + "!")
+        print("")
+        print("Your system started at " + time.strftime("%A %B %d, %Y")+ " " + time.strftime("%H:%M:%S") + " " + self.ampm)
+        print("")
+        self.overlay()
     
-if start == "n":
-  exit()
-  
-
-elif start == "y":
-    
-   
-    start1 = input("Start intellij or vscode with Chrome? ")
-    
-    #while user input is not equal to intellij or vscode, keep asking
-    while start1 != "intellij" and start1 != "vscode":
-        start1 = input("Start intellij or vscode with Chrome? ")
-        
-    if start1 == "vscode": #vscode path
+    def overlay(self):
+        start = input("Would you like to start your overlay, y or n? ")
+        while start != "y" and start != "n":
+            start = input("Would you like to start your overlay, y or n? ")
+        if start == "n":
+            exit()
+        elif start == "y":
+            print("Gotcha! ")
+            print("Starting overlay in: ")
+            countdown = 3
+            while countdown > 0:
+                print(countdown)
+                countdown = countdown - 1
+                time.sleep(1)
+                if countdown == 0:
+                    print("now!")
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.2.3.lnk")
+                    print(response)
+                    response = os.startfile(r"H:\IDEs\Microsoft VS Code\bin\code.cmd")
+                    print(response)
+                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HWiNFO64\HWiNFO64.lnk")
+                    print(response)
+                    response = os.startfile(r"C:\Users\rheyp\Desktop\Discord.lnk")
+                    print(response)
+                    break
                 
-        print ("Overlay starting in:")
-        countdown = 3  
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                        
-                response = os.startfile(r"C:\Users\Rey Halili\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk")
-                print(response)
-            
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                print(response)
-                    
-
-    elif start1 == "intellij": #intellij path
-                
-        print ("Overlay starting in:")
-        countdown = 3
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                        
-                response = os.startfile(r"C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2022.2.3\bin\idea64.exe")
-                print(response)
-                        
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                print(response)
+if __name__ == "__main__":
+    Startup()
+    
