950aef74d6cef72cebdb732968f52a61e92ccbe9
Rey Philip Louise Halili <112463409+rokujiin@users.noreply.github.com>
2023-03-17 04:45:32 -0700

    Update 2import os.py
    added error handling to the file opening sequence

--- 2import os.py
+++ 2import os.py
@@ -1,60 +1,45 @@
 import os
 import time
-from tracemalloc import start
-from urllib import response
 
-print("Welcome back Rey")
-print("")
+class Startup:
 
+    def __init__(self):
+        self.ampm = time.strftime("%p")
+        self.name = "Rey"
+        self.start()
 
-start = input("Would you like to start your overlay, y or n? ")
-
-#while user input is not equal to y or n, keep asking
-while start != "y" and start != "n":
-    start = input("Would you like to start your overlay, y or n? ")
+    def start(self):
+        print("Welcome back, " + self.name + "!")
+        print("")
+        print("Your system started at " + time.strftime("%A %B %d, %Y")+ " " + time.strftime("%H:%M:%S") + " " + self.ampm)
+        print("")
+        self.overlay()
     
-if start == "n":
-  exit()
-  
+    def overlay(self):
+        start = input("Would you like to start your overlay, y or n? ")
+        while start != "y" and start != "n":
+            start = input("Would you like to start your overlay, y or n? ")
+        if start == "n":
+            exit()
+        elif start == "y":
+            print("Gotcha! ")
+            print("Starting overlay in: ")
+            countdown = 3
+            while countdown > 0:
+                print(countdown)
+                countdown = countdown - 1
+                time.sleep(1)
+                if countdown == 0:
+                    try:
+                        with open("I:/CodeProjects/Python/Startup/app_list.txt", 'r') as file:
+                            apps = file.readlines()
+                    except FileNotFoundError:
+                        print("File not found!")
+                        exit()
 
-elif start == "y":
+                    for app in apps:
+                        app = app.strip()
+                        os.startfile(app)
+if __name__ == "__main__":
+    Startup()
     
-   
-    start1 = input("Start intellij or vscode with Chrome? ")
-    
-    #while user input is not equal to intellij or vscode, keep asking
-    while start1 != "intellij" and start1 != "vscode":
-        start1 = input("Start intellij or vscode with Chrome? ")
-        
-    if start1 == "vscode": #vscode path
-                
-        print ("Overlay starting in:")
-        countdown = 3  
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                        
-                response = os.startfile(r"C:\Users\Rey Halili\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk")
-                print(response)
-            
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                print(response)
-                    
-
-    elif start1 == "intellij": #intellij path
-                
-        print ("Overlay starting in:")
-        countdown = 3
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                        
-                response = os.startfile(r"C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2022.2.3\bin\idea64.exe")
-                print(response)
-                        
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                print(response)
