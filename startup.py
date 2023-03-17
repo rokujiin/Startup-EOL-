7e22305b68f461093b15796e20812ab24b8db67f
Rey Philip Louise Halili <112463409+rokujiin@users.noreply.github.com>
2023-03-17 04:29:49 -0700

    Update startup.py
    changed the file openeing sequence to a faster one, and added error handling to the mentioned sequence

--- startup.py
+++ startup.py
@@ -1,36 +1,45 @@
 import os
 import time
 
+class Startup:
 
-print('Welcome back, Rey')
-print('')
-start = input("Would you like to start your overlay, y or n? ")
-if start == "n":
-    exit()
-elif start == "y":   
-    print("Overlay starting in: ")
-        
-    countdown = 6
-    while countdown > 0:
-        time.sleep(1)
-        countdown = countdown - 1
-        print(countdown)
-            
-        if countdown == 0:
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-            print(response)
+    def __init__(self):
+        self.ampm = time.strftime("%p")
+        self.name = "Rey"
+        self.start()
 
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-            print(response)
-            
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.2.1.lnk")
-            print(response)
+    def start(self):
+        print("Welcome back, " + self.name + "!")
+        print("")
+        print("Your system started at " + time.strftime("%A %B %d, %Y")+ " " + time.strftime("%H:%M:%S") + " " + self.ampm)
+        print("")
+        self.overlay()
+    
+    def overlay(self):
+        start = input("Would you like to start your overlay, y or n? ")
+        while start != "y" and start != "n":
+            start = input("Would you like to start your overlay, y or n? ")
+        if start == "n":
+            exit()
+        elif start == "y":
+            print("Gotcha! ")
+            print("Starting overlay in: ")
+            countdown = 3
+            while countdown > 0:
+                print(countdown)
+                countdown = countdown - 1
+                time.sleep(1)
+                if countdown == 0:
+                    try:
+                        with open("I:/CodeProjects/Python/Startup/app_list.txt", 'r') as file:
+                            apps = file.readlines()
+                    except FileNotFoundError:
+                        print("File not found!")
+                        exit()
 
-            response = os.startfile(r"H:\IDEs\Microsoft VS Code\bin\code.cmd")
-            print(response)
-
-            response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HWiNFO64\HWiNFO64.lnk")
-            print(response)
-
-            response = os.startfile(r"C:\Users\rheyp\Desktop\Discord.lnk")
-            print(response)
+                    for app in apps:
+                        app = app.strip()
+                        os.startfile(app)
+if __name__ == "__main__":
+    Startup()
+    
