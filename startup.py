2672d930f19f24fc20af767a4c375324e579554e
Rey Philip Louise Halili <112463409+rokujiin@users.noreply.github.com>
2023-03-17 04:51:04 -0700

    Update startup.py
    added error handling to the rest of the code

--- startup.py
+++ startup.py
@@ -1,45 +1,45 @@
 import os
 import time
 
-class Startup:
 
+class Startup:
     def __init__(self):
         self.ampm = time.strftime("%p")
         self.name = "Rey"
         self.start()
 
     def start(self):
-        print("Welcome back, " + self.name + "!")
+        print(f"Welcome back, {self.name}!")
         print("")
-        print("Your system started at " + time.strftime("%A %B %d, %Y")+ " " + time.strftime("%H:%M:%S") + " " + self.ampm)
+        print(f"Your system started at {time.strftime('%A %B %d, %Y')} {time.strftime('%H:%M:%S')} {self.ampm}")
         print("")
         self.overlay()
-    
+
     def overlay(self):
         start = input("Would you like to start your overlay, y or n? ")
-        while start != "y" and start != "n":
+        while start not in ["y", "n"]:
             start = input("Would you like to start your overlay, y or n? ")
         if start == "n":
             exit()
         elif start == "y":
-            print("Gotcha! ")
-            print("Starting overlay in: ")
-            countdown = 3
-            while countdown > 0:
-                print(countdown)
-                countdown = countdown - 1
+            print("Gotcha!")
+            print("Starting overlay in:")
+            for i in range(3, 0, -1):
+                print(i)
                 time.sleep(1)
-                if countdown == 0:
-                    try:
-                        with open("I:/CodeProjects/Python/Startup/app_list.txt", 'r') as file:
-                            apps = file.readlines()
-                    except FileNotFoundError:
-                        print("File not found!")
-                        exit()
+            try:
+                with open("I:/CodeProjects/Python/Startup/app_list.txt", "r") as file:
+                    apps = file.readlines()
+            except FileNotFoundError:
+                print("File not found!")
+                exit()
 
-                    for app in apps:
-                        app = app.strip()
-                        os.startfile(app)
+            for app in apps:
+                app = app.strip()
+                try:
+                    os.startfile(app)
+                except OSError:
+                    print(f"Failed to open {app}")
+
 if __name__ == "__main__":
     Startup()
-    
