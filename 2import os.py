05442c9fb70a3b6b2b589789ccc22cd444849e85
Rey Philip Louise Halili <112463409+rokujiin@users.noreply.github.com>
2023-06-19 14:39:00 -0700

    Update 2import os.py

--- 2import os.py
+++ 2import os.py
@@ -1,49 +1,38 @@
 import os
 import time
-from tracemalloc import start
-from urllib import response
-from xmlrpc.client import ResponseError
 
-print("Welcome back Rey")
-print("")
+print("Welcome back, Rey\n")
 
-start = input("Would you like to start your overlay, y or n? ")
-if start == "n":
-  exit()
-  
+while True:
+    start = input("Would you like to start your overlay? (y/n): ")
+    if start == "n":
+        exit()
+    elif start == "y":
+        break
 
-elif start == "y":
-        
-    start1 = input("Start intellij or vscode with Chrome? ")
-    if start1 == "vscode":
-            
-        print ("Overlay starting in:")
-        countdown = 5
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                    
-                    response = os.startfile(r"C:\Users\Rey Halili\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk")
-                    print(response)
-               
-                    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                    print(response)
-                    
+while True:
+    start1 = input("Start IntelliJ or VS Code with Chrome? (intellij/vscode): ")
+    if start1 == "intellij":
+        overlay_path = r"C:\Program Files (x86)\JetBrains\IntelliJ IDEA 2022.2.3\bin\idea64.exe"
+        break
+    elif start1 == "vscode":
+        overlay_path = r"C:\Users\Rey Halili\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code\Visual Studio Code.lnk"
+        break
 
-    elif start1 == "intellij":
-                
-        print ("Overlay starting in:")
-        countdown = 5
-        while countdown > 0:
-            print (countdown)
-            countdown = countdown - 1
-            time.sleep(1)
-            if countdown == 0:
-                        
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\JetBrains\IntelliJ IDEA 2022.2.1.lnk")
-                print(response)
-                        
-                response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk")
-                print(response)
+chrome_path = r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk"
+
+print("\nOverlay starting in:")
+try:
+    for countdown in range(3, 0, -1):
+        print(countdown)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Program canceled")
+    exit()
+
+response = os.startfile(overlay_path)
+print(response)
+
+response = os.startfile(chrome_path)
+print(response)
+
